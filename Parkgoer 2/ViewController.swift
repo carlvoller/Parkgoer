fe17107f2016e3d6c1ebfc562e32d83cbe624bbb
Carl Ian Voller <carlvoller8@gmail.com>
2018-10-29 11:45:14 +0800

    Initial Commit

---
+++ Parkgoer 2/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Parkgoer 2
+//
+//  Created by Carl Ian Voller on 29/10/18.
+//  Copyright © 2018 CodeSpace Inc. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ Parkgoer 2Tests/Parkgoer_2Tests.swift
@@ -0,0 +1,34 @@
+//
+//  Parkgoer_2Tests.swift
+//  Parkgoer 2Tests
+//
+//  Created by Carl Ian Voller on 29/10/18.
+//  Copyright © 2018 CodeSpace Inc. All rights reserved.
+//
+
+import XCTest
+@testable import Parkgoer_2
+
+class Parkgoer_2Tests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
