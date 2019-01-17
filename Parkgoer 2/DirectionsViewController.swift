f8483957d16a326ef31bbfe6a6fa42b84fc8d26b
Carl Ian Voller <carlvoller8@gmail.com>
2019-01-17 21:46:06 +0800

    Initial commit

---
+++ Parkgoer 2/DirectionsViewController.swift
@@ -0,0 +1,30 @@
+//
+//  DirectionsViewController.swift
+//  Parkgoer 2
+//
+//  Created by Carl Ian Voller on 29/10/18.
+//  Copyright © 2018 CodeSpace Inc. All rights reserved.
+//
+
+import UIKit
+
+class DirectionsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ Parkgoer 2/Park.swift
@@ -0,0 +1,9 @@
+//
+//  Park.swift
+//  Parkgoer 2
+//
+//  Created by Carl Ian Voller on 29/10/18.
+//  Copyright © 2018 CodeSpace Inc. All rights reserved.
+//
+
+import Foundation
